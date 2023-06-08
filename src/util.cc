e89d080dbdde4b4e725d6ee1b108bd0b856fd41e
handsomeDHT <707303050@qq.com>
2023-06-08 14:26:19 +0800

    携程 01 done

--- src/util.cc
+++ src/util.cc
@@ -2,15 +2,47 @@
 // Created by 帅帅的涛 on 2023/3/29.
 //
 
+#include <execinfo.h>
 #include "util.h"
+#include "log.h"
 
 namespace dht{
+
+static dht::Logger::ptr g_logger = DHT_LOG_NAME("system");
 
 pid_t GetThreadId() {
     return syscall(SYS_gettid);
 }
 u_int32_t GetFiberId(){
     return 0;
+}
+
+void Backtrace(std::vector<std::string>& bt, int size, int skip) {
+    void** array = (void**)malloc((sizeof(void*) * size));
+    size_t s = ::backtrace(array, size);
+
+    char** strings = backtrace_symbols(array, s);
+    if(strings == NULL) {
+        DHT_LOG_ERROR(g_logger) << "backtrace_synbols error";
+        return;
+    }
+
+    for(size_t i = skip; i < s; ++i) {
+        bt.push_back(strings[i]);
+    }
+
+    free(strings);
+    free(array);
+}
+
+std::string BacktraceToString(int size, int skip, const std::string& prefix) {
+    std::vector<std::string> bt;
+    Backtrace(bt, size, skip);
+    std::stringstream ss;
+    for(size_t i = 0; i < bt.size(); ++i) {
+        ss << prefix << bt[i] << std::endl;
+    }
+    return ss.str();
 }
 
 }
--- src/util.h
+++ src/util.h
@@ -6,11 +6,29 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/syscall.h>
+#include <string>
+#include <vector>
 
 namespace dht {
 
 pid_t GetThreadId();
 u_int32_t GetFiberId();
+
+/**
+ * @brief 获取当前的调用栈
+ * @param[out] bt 保存调用栈
+ * @param[in] size 最多返回层数
+ * @param[in] skip 跳过栈顶的层数
+ */
+void Backtrace(std::vector<std::string>& bt, int size = 64, int skip = 1);
+
+/**
+ * @brief 获取当前栈信息的字符串
+ * @param[in] size 栈的最大层数
+ * @param[in] skip 跳过栈顶的层数
+ * @param[in] prefix 栈信息前输出的内容
+ */
+std::string BacktraceToString(int size = 64, int skip = 2, const std::string& prefix = "");
 
 }
 #endif //SERVER_FRAMWORK_UTIL_H
