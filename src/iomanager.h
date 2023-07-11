cb27ed4a8be25d21fde8a59c84c86ceb7bd8ae78
handsomeDHT <707303050@qq.com>
2023-07-11 21:52:30 +0800

    io携程 03 done

--- src/iomanager.h
+++ src/iomanager.h
@@ -1,3 +1,8 @@
+/**
+ * 基于Epoll的IO携程调度器
+ */
+
+
 #ifndef SERVER_FRAMWORK_IOMANAGER_H
 #define SERVER_FRAMWORK_IOMANAGER_H
 
@@ -10,25 +15,36 @@ public:
     typedef std::shared_ptr<IOManager> ptr;
     typedef RWMutex RWMUtexType;
 
+    //IO事件
     enum Event {
+        //无事件
         NONE = 0x0,
+        //读事件(EPOLLIN)
         READ = 0x1,
+        //写事件(EPOLLOUT)
         WRITE = 0x2
     };
 private:
+    //Socket事件上线文类
     struct FdContext{
         typedef Mutex MutexType;
+        //事件上线文类
         struct EventContext{
             Scheduler* scheduler = nullptr;     //待执行的scheduler
             Fiber::ptr fiber;         //事件协程
             std::function<void()> cb; //事件的回调函数
         };
 
+        /**
+         * @brief 获取事件上下文类
+         * @param[in] event 事件类型
+         * @return 返回对应事件的上线文
+         */
         EventContext& getContext(Event event);
+        //重置事件上下文(ctx->待重置的上下文类)
         void resetContext(EventContext& ctx);
+        //触发事件
         void triggerEvent(Event event);
-
-
 
         EventContext read;//读事件
         EventContext write;//写事件
@@ -43,11 +59,17 @@ public:
                 ,const std::string& name = "");
     ~IOManager();
 
-    //0 success, -1 error
+    /**
+     * @brief 添加事件
+     * @param[in] fd socket句柄
+     * @param[in] event 事件类型
+     * @param[in] cb 事件回调函数
+     * @return 添加成功返回0,失败返回-1
+     */
     int addEvent(int fd, Event event, std::function<void()> cb = nullptr);
     bool delEvent(int fd, Event event);
-    bool cancelEvent(int fd, Event event);
 
+    bool cancelEvent(int fd, Event event);
     bool cancelAll(int fd);
 
     static IOManager* GetThis();
@@ -59,11 +81,14 @@ protected:
 
     void contextResize(size_t size);
 private:
+    //epoll 文件句柄
     int m_epfd = 0;
+    // pipe文件句柄
     int m_tickleFds[2];
-
+    //当前等待执行的事件数量
     std::atomic<size_t> m_pendingEventCount = {0};
     RWMUtexType m_mutex;
+    //socket事件上下文的容器
     std::vector<FdContext*> m_fdContexts;
 };
 
